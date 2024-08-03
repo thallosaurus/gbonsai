3c18c4b3ee3dbe9d9ba93ea271535af7cabc83b2
Philipp Gaßner <activator112233@gmail.com>
2024-08-03 23:12:58 +0200

    build now

--- web/vue/src/App.vue
+++ web/vue/src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
+//import { RouterLink } from 'vue-router';
 //import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <nav>
-    <!--
     <RouterLink to="/">Home</RouterLink>
+    <!--
     <RouterLink to="/bonsai">Bonsai</RouterLink>
     -->
   </nav>
